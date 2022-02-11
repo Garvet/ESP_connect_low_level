07a12aa92ce2d1cb6724a00123a965c52050d85b
Garvet <Mitriel-leirtim@mail.ru>
2022-02-11 17:08:23 +0300

    Added library with additional functionality

---
+++ Additional_functionality/Additional_functionality.cpp
@@ -0,0 +1,26 @@
+#include "Additional_functionality.hpp"
+
+uint16_t convert_to_hex(uint8_t byte) {
+    static const char name[16] {'0', '1', '2', '3', '4', '5', '6',
+        '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    uint16_t result = ((uint16_t)name[(byte>>4)&0xF]) << 8 | ((uint16_t)name[byte&0xF]);
+    return result;
+}
+
+size_t convert_to_hex(const uint8_t* where, uint8_t* whence, size_t len) {
+    for (size_t i = 0; i < len; i++) {
+        whence[i * 2]     = (where[i] >> 4) & 0x0F;
+        whence[i * 2 + 1] =  where[i]       & 0x0F;
+        if(whence[i * 2] < 10) whence[i * 2] += '0';
+        else                   whence[i * 2] += 'A' - 10;
+        if(whence[i * 2 + 1] < 10) whence[i * 2 + 1] += '0';
+        else                       whence[i * 2 + 1] += 'A' - 10;
+    }
+    return len * 2;
+}
+
+size_t convert_to_hex(const uint8_t* where, char* whence, size_t len) {
+    return convert_to_hex(where, (uint8_t*) whence, len);
+    Serial.p
+    for()
+}
---
+++ Additional_functionality/Additional_functionality.hpp
@@ -0,0 +1,20 @@
+#ifndef ADDITIONAL_FUNCTIONALITY_HPP_
+#define ADDITIONAL_FUNCTIONALITY_HPP_
+
+template<typename T, unsigned int N>
+constexpr unsigned int array_size(T(&)[N]) noexcept {
+    return N;
+}
+
+#if defined( linux )
+#include "../Arduino/Arduino.h"
+#endif // linux
+
+// NUM -> 0x?? ; 0x3C -> ('3' << 8 | 'C')
+uint16_t convert_to_hex(uint8_t byte);
+// where - NUM[len] ; whence - STR[len * 2]
+size_t convert_to_hex(const uint8_t* where, uint8_t* whence, size_t len);
+inline size_t convert_to_hex(const uint8_t* where, char* whence, size_t len);
+
+
+#endif // ADDITIONAL_FUNCTIONALITY_HPP_
